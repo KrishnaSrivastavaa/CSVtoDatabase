20cf5ab3e68be6fed62fb41915aa9f4de3a839ea
Krishna Srivastava <87747154+KrishnaSrivastavaa@users.noreply.github.com>
2023-08-25 18:43:41 +0530

    Update etl_pipeline.py

--- src/etl_pipeline.py
+++ src/etl_pipeline.py
@@ -3,14 +3,15 @@ import csv
 import logging
 import configparser
 import os
+from datetime import datetime
 
-
+# Set up logging
 logging.basicConfig(filename=os.path.join(os.path.dirname(__file__), '..', 'logs', 'etl_log.log'),
-                    level=logging.INFO,  
+                    level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
 
-
+# Read configuration from config.ini
 config = configparser.ConfigParser()
 config.read(os.path.join(os.path.dirname(__file__), '..', 'config', 'config.ini'))
 
@@ -19,14 +20,15 @@ class ETLPipeline:
         self.csv_file = csv_file
 
     def _transform_data(self, row):
-        
-        full_name = f"{row['first_name']} {row['last_name']}"
-        row['full_name'] = full_name
+        # Convert dates to proper format
+        row['Join Date'] = datetime.strptime(row['Join Date'], '%Y-%m-%d').date()
+        row['Date of Birth'] = datetime.strptime(row['Date of Birth'], '%Y-%m-%d').date()
+
         return row
 
     def run(self):
         try:
-          
+            # Establish database connection
             conn = psycopg2.connect(
                 host=config['postgresql']['host'],
                 database=config['postgresql']['database'],
@@ -35,28 +37,32 @@ class ETLPipeline:
             )
             cursor = conn.cursor()
 
-           
+            # Create table if it doesn't exist
             create_table_query = '''
             CREATE TABLE IF NOT EXISTS employees (
                 id SERIAL PRIMARY KEY,
                 first_name VARCHAR,
                 last_name VARCHAR,
-                full_name VARCHAR,
-                email VARCHAR,
-                department VARCHAR
+                employee_id VARCHAR,
+                manager_name VARCHAR,
+                join_date DATE,
+                date_of_birth DATE,
+                employee_age INTEGER,
+                employee_salary INTEGER,
+                employee_department VARCHAR
             );
             '''
             cursor.execute(create_table_query)
             conn.commit()
 
-          
+            # Read CSV file and insert data into the database
             with open(os.path.join(os.path.dirname(__file__), '..', 'data', self.csv_file), 'r') as csv_file:
                 csv_reader = csv.DictReader(csv_file)
                 for row in csv_reader:
                     transformed_row = self._transform_data(row)
                     insert_query = '''
-                    INSERT INTO employees (first_name, last_name, full_name, email, department)
-                    VALUES (%(first_name)s, %(last_name)s, %(full_name)s, %(email)s, %(department)s);
+                    INSERT INTO employees (first_name, last_name, employee_id, manager_name, join_date, date_of_birth, employee_age, employee_salary, employee_department)
+                    VALUES (%(Firstname)s, %(Lastname)s, %(Employee ID)s, %(Employee Manager Name)s, %(Join Date)s, %(Date of Birth)s, %(Employee Age)s, %(Employee Salary)s, %(Employee Department Name)s);
                     '''
                     cursor.execute(insert_query, transformed_row)
                     conn.commit()
